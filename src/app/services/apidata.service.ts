c97d7d10402de62a6d561ca3c838344131843f93
adelalrafiq <adelalrafiq@gmail.com>
2021-01-20 21:04:42 +0100

    updated https

--- src/app/services/apidata.service.ts
+++ src/app/services/apidata.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {  HttpClient } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { pluck } from 'rxjs/operators';
 
@@ -8,20 +8,20 @@ import { pluck } from 'rxjs/operators';
 })
 export class ApidataService {
 
- /*  private message = new Subject<any>(); */
+  /*  private message = new Subject<any>(); */
 
   constructor(private http: HttpClient) { }
 
   //call api
-   getPosts(){
-    return this.http.get('http://api.irail.be/connections/?from=Gent-Dampoort&to=Sint-Niklaas&format=json');
+  getPosts() {
+    return this.http.get('https://api.irail.be/connections/?from=Gent-Dampoort&to=Sint-Niklaas&format=json');
   }
 
-  getPostsFrom(from, to){
+  getPostsFrom(from, to) {
     console.log(from, to)
-    return this.http.get(`http://api.irail.be/connections/?from=${from}&to=${to}&format=json`);
+    return this.http.get(`https://api.irail.be/connections/?from=${from}&to=${to}&format=json`);
   }
-  getAllStations(): Observable<any>{
-    return this.http.get('http://api.irail.be/stations/?format=json&lang=en').pipe(pluck('station'))
-  }  
+  getAllStations(): Observable<any> {
+    return this.http.get('https://api.irail.be/stations/?format=json&lang=en').pipe(pluck('station'))
+  }
 }
